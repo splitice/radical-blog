1349ab4c7692b08c42533f300928cd7fda5bc998
SplitIce <mat999@gmail.com>
2012-04-02 17:41:21 +1000

    Config Setup

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+$_CONFIG = array();
+
+//$_BACKUP = array('to'=>'email');
+
+$_SQL = new Database\DBAL\Adapter\Connection('db', 'radical_blog', 'radical_blog', 'radical_blog');
+
+$_PROJECT = 'Blog';
