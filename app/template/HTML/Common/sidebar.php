8dc44dd685744a00029a160850e3c8352f825c9f
SplitIce <mat999@gmail.com>
2012-04-14 02:27:28 +1000

    sidebar

---
+++ app/template/HTML/Common/sidebar.php
@@ -0,0 +1,113 @@
+<div class="sidebar">
+	<div class="sidebar_box">
+		<h4>Theme-Switcher</h4>
+		<ul id="themeswitcher">
+			<li><a
+				href="http://themes.koch-werkstatt.de/index.php?wptheme=Experimental">Experimental</a></li>
+			<li><a
+				href="http://themes.koch-werkstatt.de/index.php?wptheme=ModiOne">ModiOne</a></li>
+			<li><a
+				href="http://themes.koch-werkstatt.de/index.php?wptheme=ModiTwo">ModiTwo</a></li>
+			<li>nameless</li>
+			<li><a
+				href="http://themes.koch-werkstatt.de/index.php?wptheme=SimplicityBright">SimplicityBright</a></li>
+			<li><a
+				href="http://themes.koch-werkstatt.de/index.php?wptheme=SimplicityDark">SimplicityDark</a></li>
+		</ul>
+	</div>
+
+	<div class="sidebar_box">
+		<h4>Pages</h4>
+		<ul>
+			<li class="current_page_item"><a
+				href="http://themes.koch-werkstatt.de">Home</a></li>
+			<li class="page_item page-item-227"><a
+				href="http://themes.koch-werkstatt.de/datenschutz/"
+				title="Datenschutz">Datenschutz</a></li>
+			<li class="page_item page-item-12"><a
+				href="http://themes.koch-werkstatt.de/impressum/" title="Impressum">Impressum</a></li>
+		</ul>
+	</div>
+
+	<div class="sidebar_box">
+		<h4>Categories</h4>
+		<ul>
+			<li class="cat-item cat-item-1"><a
+				href="http://themes.koch-werkstatt.de/category/allgemein/"
+				title="Alle unter Allgemein abgelegten Artikel ansehen">Allgemein</a>
+			</li>
+			<li class="cat-item cat-item-76"><a
+				href="http://themes.koch-werkstatt.de/category/design/"
+				title="Alle unter Design abgelegten Artikel ansehen">Design</a></li>
+			<li class="cat-item cat-item-4"><a
+				href="http://themes.koch-werkstatt.de/category/fotos/"
+				title="Alle unter Fotos abgelegten Artikel ansehen">Fotos</a></li>
+			<li class="cat-item cat-item-10"><a
+				href="http://themes.koch-werkstatt.de/category/text/"
+				title="Alle unter Text abgelegten Artikel ansehen">Text</a></li>
+		</ul>
+	</div>
+
+
+	<div class="sidebar_box">
+		<h4>Archive</h4>
+		<ul>
+			<li><a href='http://themes.koch-werkstatt.de/2010/03/'
+				title='März 2010'>März 2010</a></li>
+			<li><a href='http://themes.koch-werkstatt.de/2008/11/'
+				title='November 2008'>November 2008</a></li>
+			<li><a href='http://themes.koch-werkstatt.de/2008/09/'
+				title='September 2008'>September 2008</a></li>
+			<li><a href='http://themes.koch-werkstatt.de/2007/04/'
+				title='April 2007'>April 2007</a></li>
+		</ul>
+	</div>
+
+
+	<div class="sidebar_box">
+		<h4>Search</h4>
+		<div id="searchthis">
+			<form method="get" id="searchform"
+				action="http://themes.koch-werkstatt.de/">
+				<div>
+					<input type="text" value="" name="s" id="s" /> <input
+						id="searchsubmit" type="image"
+						src="http://themes.koch-werkstatt.de/wp-content/themes/nameless/images/search.gif"
+						alt="Submit" />
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+	<div class="sidebar_box">
+		<h4>Blogroll</h4>
+		<ul>
+			<li><a href="http://blogmap.wordpress-deutschland.org/">Blogmap</a></li>
+			<li><a href="http://doku.wordpress-deutschland.org/">Dokumentation</a></li>
+			<li><a href="http://faq.wordpress-deutschland.org/">FAQ</a></li>
+			<li><a href="http://blog.wordpress-deutschland.org/">News-Blog</a></li>
+			<li><a href="http://forum.wordpress-deutschland.org/">Support-Forum</a></li>
+			<li><a href="http://themes.wordpress-deutschland.org/">Themepool</a></li>
+			<li><a href="http://planet.wordpress-deutschland.org/">WordPress-Planet</a></li>
+		</ul>
+	</div>
+
+
+	<div class="sidebar_box">
+		<h4>Meta</h4>
+		<ul>
+			<li><a href="http://themes.koch-werkstatt.de/wp-login.php">Anmelden</a></li>
+			<li><a href="http://wordpress.org/"
+				title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress</a></li>
+		</ul>
+	</div>
+
+	<div class="sidebar_box">
+		<h4>Tipp</h4>
+		<ul>
+			<li><a href="http://www.mozilla-europe.org/de/">If it looks weird in
+					your browser, get a better one</a></li>
+		</ul>
+	</div>
+</div>
