187ae1846af596b382e0087ed35996e0437bc041
SplitIce <mat999@gmail.com>
2012-04-24 20:37:35 +1000

    Temporary Alphabetical order work around.

--- app/lib/ExampleBlog/Web/PageRecogniser/Recognisers/APost.php
+++ app/lib/ExampleBlog/Web/PageRecogniser/Recognisers/APost.php
@@ -1,7 +1,7 @@
 <?php
 namespace Web\PageRecogniser\Recognisers;
 
-class Post extends Templates\Standard {
+class APost extends Templates\Standard {
 	static $match = array(
 		'/p/%(post)s/'=>'\\Web\\Pages\\Post',
 		'/p/%(post)s'=>'\\Web\\Pages\\Post',
