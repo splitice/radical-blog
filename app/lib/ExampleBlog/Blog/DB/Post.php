d026e5b19ec7d74e33c884cdf8bf7a5962736bb9
SplitIce <mat999@gmail.com>
2012-05-14 00:15:27 +1000

    ... I cant remember what I changed.

--- app/lib/ExampleBlog/Blog/DB/Post.php
+++ app/lib/ExampleBlog/Blog/DB/Post.php
@@ -1,11 +1,9 @@
 <?php
 namespace Blog\DB;
+
 use Blog\CommentForm;
-
 use Basic\Arr;
-
 use Basic\String\Truncate;
-
 use Database\Model\Table;
 
 /**
@@ -18,18 +16,19 @@ class Post extends Table {
 	const TABLE_PREFIX = 'post_';
 	const TABLE = 'post';
 	
-	protected $id;
-	protected $title;
+	/* Database Fields */
+	protected $id;//post_id
+	protected $title;//post_title
 	
 	/**
 	 * The post body
 	 */
-	protected $content;
+	protected $content;//post_content
 	
 	/**
 	 * The user who posted the post. aka the poster.
 	 */
-	protected $user;
+	protected $user;//user_id (Foreign Key to users table)
 	
 	/**
 	 * Date field, althout the DynamicType class would be applied
@@ -37,16 +36,16 @@ class Post extends Table {
 	 * 
 	 * @var DateTime
 	 */
-	protected $date;
+	protected $date;//post_date
 	
 	/**
 	 * The URL Stub used to access a post.
 	 * 
 	 * @var Stub
 	 */
-	protected $stub;
+	protected $stub;//post_stub
 	
-	protected $category;
+	protected $category;//category_id (A Foreign key to the category table)
 	
 	/**
 	 * Get a Truncated version of the Content.
@@ -63,20 +62,24 @@ class Post extends Table {
 		return substr($this->content,0,$pos);
 	}
 	
+	/* An optional user defined function */
 	function getTags(){
 		return Arr::map(function(PostTag $pt){
 			return $pt->getTag();
 		}, $this->getPostTags());
 	}
 	
+	/* Optional: The URL to the models main page */
 	function toURL(){
 		return '/p/'.$this->stub;
 	}
 	
+	/* Optional: A form directly related to the model */
 	function getCommentFormBuilder($event){
 		return new CommentForm($this,$event);
 	}
 	
+	/* Optional: Specialized selector */
 	static function fromStub($stub){
 		return static::fromFields(array('*stub'=>$stub));
 	}
